43b76ecebad0153aca01687b40555dc56a663328
Prashant Misra <98226143+Prashant2623@users.noreply.github.com>
2023-01-29 14:28:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,21 @@
-# security credentials
 provider "aws"{
 region = "us-east-1"
 }
 
-# variables declaration
 variable  vpc_cidr_block {}
 variable  subnet_cidr_block {}
 variable  avail_zone {}  
 variable  env_prefix {}
 variable instance_type {}
 variable my_ip {}
-variable my_public_key {}
 
-# VPC
 resource "aws_vpc" "myapp-vpc" {
   cidr_block =  var.vpc_cidr_block
   tags = {
     Name: "${var.env_prefix}-vpc"
   }
 }
-
-# SUBNET 
+ 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
@@ -30,7 +25,6 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 
-# ROUTE TABLE AND INTERNET GATEWAY
 resource "aws_default_route_table" "main-rtb" {
   default_route_table_id =  aws_vpc.myapp-vpc.default_route_table_id
    route {
@@ -41,6 +35,7 @@ resource "aws_default_route_table" "main-rtb" {
       Name: "${var.env_prefix}-main-rtb"
    }
 }
+
 resource "aws_internet_gateway" "myapp-igw" {
    vpc_id = aws_vpc.myapp-vpc.id
    tags = {
@@ -48,23 +43,22 @@ resource "aws_internet_gateway" "myapp-igw" {
    }
 }
 
-# SECURITY GROUP
 resource "aws_default_security_group" "default-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id  #to reference, use the vpc
+  vpc_id = aws_vpc.myapp-vpc.id  
 
-ingress {                                      #incoming traffic rule
+ingress {                                      
   from_port = 22             
   to_port = 22
   protocol = "tcp"
   cidr_blocks = [var.my_ip]
 }
-ingress {                                      #incoming traffic rule
+ingress {                                    
   from_port = 8080                
   to_port = 8080
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]          
  }
-egress {                                      #outgoing traffic rule
+egress {                                      
   from_port = 0                
   to_port = 0
   protocol = "-1"
@@ -76,7 +70,6 @@ egress {                                      #outgoing traffic rule
    }  
 }
 
-# EC2 INSTANCE
 data "aws_ami" "latest-amazon-linux-image" {
   most_recent = true
   owners = ["137112412989"]
@@ -92,7 +85,7 @@ data "aws_ami" "latest-amazon-linux-image" {
 
 resource "aws_key_pair" "ssh-key" {
 key_name = "server-key"
-public key = "var.my_public_key"
+public_key = "var.my_public_key"
 }
 
 resource "aws_instance" "myapp-server" {
@@ -101,12 +94,10 @@ resource "aws_instance" "myapp-server" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   availability_zone = var.avail_zone
-  associate_public_ip_address = true   # access the server from the browser
-  key_name =  aws_key_pair.ssh-key.key_name  
+  associate_public_ip_address = true   
+  key_name = "Terraform"   
   user_data = file("entry-script.sh")
 tags = {
       Name: "${var.env_prefix}-server"
    }   
 }
-
-
